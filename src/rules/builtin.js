fc2c3535c794b72bbf6c74f5e34ca7258c485338
Manuel Pacheco <contact@manuelpacheco.dev>
2022-05-01 17:44:48 -0400

    add builtin possible problem eslint rules

--- src/rules/builtin.js
+++ src/rules/builtin.js
@@ -19,4 +19,59 @@ module.exports = mapOptionsAsErrors({
   "no-control-regex": true,
   "no-debugger": true,
   "no-dupe-args": true,
+  "no-dupe-class-members": true,
+  "no-dupe-else-if": true,
+  "no-dupe-keys": true,
+  "no-duplicate-case": true,
+  "no-duplicate-imports": { includeExports: false },
+  "no-empty-character-class": true,
+  "no-empty-pattern": true,
+  "no-ex-assign": true,
+  "no-fallthrough": true,
+  "no-func-assign": true,
+  "no-import-assign": true,
+  "no-inner-declarations": "both",
+  "no-invalid-regexp": true,
+  "no-irregular-whitespace": { skipStrings: true },
+  "no-loss-of-precision": true,
+  "no-misleading-character-class": true,
+  "no-new-symbol": true,
+  "no-obj-calls": true,
+  "no-promise-executor-return": true,
+  "no-prototype-builtins": true,
+  "no-self-assign": { props: true },
+  "no-self-compare": true,
+  "no-setter-return": true,
+  "no-sparse-arrays": true,
+  "no-template-curly-in-string": true,
+  "no-this-before-super": true,
+  "no-undef": { typeof: false },
+  "no-unexpected-multiline": true,
+  "no-unmodified-loop-condition": true,
+  "no-unreachable": true,
+  "no-unreachable-loop": true,
+  "no-unsafe-finally": true,
+  "no-unsafe-negation": { enforceForOrderingRelations: true },
+
+  // TypeScript comments added from here
+  // Rules above may be disabled for TypeScript source code for performance.
+
+  // NOTE: Not needed for TypeScript (strict)
+  "no-unsafe-optional-chaining": { disallowArithmeticOperators: true },
+  // NOTE: Not needed for TypeScript (noUnusedLocals)
+  "no-unused-private-class-members": true,
+  // NOTE: Not needed for TypeScript (noUnusedLocals, noUnusedParameters)
+  "no-unused-vars": {
+    vars: "all",
+    args: "after-used",
+    ignoreRestSiblings: true,
+  },
+  // NOTE: Not needed for TypeScript
+  // But it could be useful for forbidding only function hoisting
+  "no-use-before-define": { functions: true, classes: true, variables: true },
+  "no-useless-backreference": true,
+  "require-atomic-updates": { allowProperties: false },
+  "use-isnan": { enforceForSwitchCase: true, enforceForIndexOf: true },
+  // NOTE: Not needed for TypeScript
+  "valid-typeof": true,
 });
